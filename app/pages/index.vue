7735ce4a89c3b7bb4104229c796b88732ed8f707
NyllRE <nyll.redeast@gmail.com>
2026-01-24 19:16:56 +0300

    redesign home page

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,39 +1,51 @@
 <!-- @format -->
 
 <template>
-	<UContainer>
+	<UContainer class="max-w-3xl">
 
-		<h1 class="my-6 text-2xl text-center">Welcome to Our Application</h1>
+		<h1 class="my-6 text-2xl text-center">Dynamic Form Engine Demo</h1>
+		<img
+			class="mx-auto mb-6 rounded-xl w-xl"
+			src="/dynamic-form-engine-banner.svg"
+			alt="App Logo"
+		/>
 		<p class="text-body1 text-center">
-			This is the main landing page. Use the navigation menu to explore different sections of the app.
+			This is a demo application for the
+			<strong>Dynamic Form Engine</strong> Nuxt layer. You can explore different
+			demos showcasing the capabilities of the form engine by clicking the buttons
+			below.
 		</p>
 
 
-		<div class="flex justify-center gap-5">
+		<div class="flex flex-col justify-center gap-5 mx-auto my-4 w-auto md:w-lg">
 
 			<UButton
-			class="my-4"
+			class="flex justify-center items-center"
 			to="/house-demo"
-			color="primary"
+			color="info"
+			size="xl"
 			variant="soft"
 			label="House Demo"
-			> House Demo </UButton>
+			> House purchase demo </UButton>
 
 			<UButton
-			class="my-4"
+			class="flex justify-center items-center"
+			size="xl"
 			to="/showcase"
 			color="primary"
 			variant="soft"
 			label="Showcase"
-			> Showcase </UButton>
+			> Multiple screen types demo </UButton>
+
 
 			<UButton
-			class="my-4"
+			class="flex justify-center items-center"
 			color="neutral"
+			size="xl"
 			variant="soft"
 			label="About"
 			@click="store.resetAll();"
-			> Reset Stores </UButton>
+			> Reset Forms </UButton>
 		</div>
 
 	</UContainer>

848e860d60f0e999056ac1dee1dffcd56b8a7ac6
NyllRE <nyll.redeast@gmail.com>
2026-02-08 10:23:38 +0300

    feat: Implement SEO metadata for the application and integrate oxlint for code linting.

--- app/app.vue
+++ app/app.vue
@@ -1,4 +1,15 @@
 <!-- @format -->
+<script setup lang="ts">
+
+useSeoMeta({
+	title: 'Dynamic Form Engine Demo App',
+	description:
+		'Explore the capabilities of the Dynamic Form Engine through various demos showcasing dynamic forms, multi-step processes, and more.',
+	twitterImage: '/dynamic-form-engine-banner.svg',
+
+})
+
+</script>
 
 <template>
 	<UApp>
